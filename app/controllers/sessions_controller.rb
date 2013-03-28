178303211e5a107d7ed15f48537720a5cdaf045e
Colin <colin@woofound.com>
2013-03-28 18:08:49 -0400

    Made users rememberable and changed current_user helper to do an auth_token lookup instead of id

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,11 @@ class SessionsController < ApplicationController
 	def create
 	  user = User.find_by_identifier(params[:identifier])
 	  if user && user.authenticate(params[:password])
-		  session[:user_id] = user.id
+	  	if params[:remember_me]
+	  		cookies.permanent[:auth_token] = user.auth_token
+	  	else
+	  		cookies[:auth_token] = user.auth_token
+	  	end
 			redirect_to root_url, :notice => "Logged In!"
 	  else
 		  flash[:alert] = "Invalid Identifier Or Password"
@@ -17,7 +21,7 @@ class SessionsController < ApplicationController
 	end
 
 	def destroy
-	  session[:user_id] = nil
+	  cookies.delete(:auth_token)
 		redirect_to root_url, :notice => "Logged Out!"
 	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ActiveRecord::Base
   attr_accessor :password
 
   before_save :hash_password
+  before_create { generate_token(:auth_token) }
 
   validates_confirmation_of :password
   validates :password, :presence => { :on => :create }, :confirmation => true
@@ -19,12 +20,21 @@ class User < ActiveRecord::Base
     where("username = ? OR email = ?", identifier, identifier).first
   end
 
+  # Check the supplied password against the stored hash
   def authenticate(password)
     hashed_password == BCrypt::Engine.hash_secret(password, salt)
   end
 
   private
 
+  # Generate a unique random token for 'column' field
+  def generate_token(column)
+    begin
+      self[column] = SecureRandom.urlsafe_base64
+    end while User.exists?(column => self[column])
+  end
+
+  # Hash the users password and generate a salt on creation
   def hash_password
     if password.present?
       self.salt = BCrypt::Engine.generate_salt
