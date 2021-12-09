04e3909044774bdc48cfe6bb9b5460355d80a5ac
4bstractor <theunderstander@gmail.com>
2021-12-08 23:57:50 -0500

    Added postgres extension to schema

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_12_09_035822) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "members", id: false, force: :cascade do |t|
     t.integer "user_id"
     t.integer "story_id"
